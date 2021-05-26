822fe7c623c1626953e6716c1596526daf386399
breno39 <64810645+breno39@users.noreply.github.com>
2021-05-25 23:36:15 -0300

    first commit

---
+++ src/main/java/dev/tribos/wakandacity/WakandaCityApplication.java
@@ -0,0 +1,13 @@
+package dev.tribos.wakandacity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakandaCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakandaCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tribos/wakandacity/WakandaCityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tribos.wakandacity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakandaCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
