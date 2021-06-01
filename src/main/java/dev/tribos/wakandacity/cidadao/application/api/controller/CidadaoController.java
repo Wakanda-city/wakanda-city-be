cdf3fa07d730e4949827ec2234ba88e99d836b19
beker26 <rodrigobeker18@gmail.com>
2021-06-01 10:40:50 -0300

    Implementando os logs no método preCadastoCidadão no controller e service

--- src/main/java/dev/tribos/wakandacity/cidadao/application/api/controller/CidadaoController.java
+++ src/main/java/dev/tribos/wakandacity/cidadao/application/api/controller/CidadaoController.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-
 import dev.tribos.wakandacity.cidadao.application.api.data.request.CidadaoForm;
 import dev.tribos.wakandacity.cidadao.application.api.data.response.CidadaoDTO;
 import dev.tribos.wakandacity.cidadao.domain.Cidadao;
 import dev.tribos.wakandacity.cidadao.service.CidadaoService;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @RestController
 @RequestMapping("/v1/cidadao")
 public class CidadaoController implements CidadaoAPI {
@@ -28,8 +29,10 @@ public class CidadaoController implements CidadaoAPI {
 	@Override
 	public ResponseEntity<CidadaoDTO> preCadastraCidadao(@Valid CidadaoForm cidadaoForm,
 			UriComponentsBuilder uriBuilder) {
-
+		log.info("Starting Method preCadastraCidadao in CidadaoController!");
+		log.info("Form: {}", cidadaoForm);
 		Cidadao cidadao = cidadaoService.preCadastraCidadao(cidadaoForm);
+		log.info("Finishing Method  preCadastraCidadao in CidadaoController!");
 		URI uri = uriBuilder.path("/cidadao/{id}").buildAndExpand(cidadao.getId()).toUri();
 		return ResponseEntity.created(uri).body(new CidadaoDTO(cidadao));
 	}
--- src/main/java/dev/tribos/wakandacity/cidadao/service/impl/CidadaoSpringDataJPAServiceImpl.java
+++ src/main/java/dev/tribos/wakandacity/cidadao/service/impl/CidadaoSpringDataJPAServiceImpl.java
@@ -6,8 +6,10 @@ import dev.tribos.wakandacity.cidadao.Repository.CidadaoRepository;
 import dev.tribos.wakandacity.cidadao.application.api.data.request.CidadaoForm;
 import dev.tribos.wakandacity.cidadao.domain.Cidadao;
 import dev.tribos.wakandacity.cidadao.service.CidadaoService;
+import lombok.extern.log4j.Log4j2;
 
 @Service
+@Log4j2
 public class CidadaoSpringDataJPAServiceImpl implements CidadaoService {
 
 	private CidadaoRepository cidadaoRepository;
@@ -18,7 +20,9 @@ public class CidadaoSpringDataJPAServiceImpl implements CidadaoService {
 
 	@Override
 	public Cidadao preCadastraCidadao(CidadaoForm cidadaoForm) {
+		log.info("Starting Method insert in CidadaoSpringDataJPAServiceImpl");
 		Cidadao cidadao = cidadaoForm.toCidadao();
+		log.info("finishing Method insert in CidadaoSpringDataJPAServiceImpl");
 		return cidadaoRepository.save(cidadao);
 	}
 
